26fe0eb56b17b4a47fe9c880dc8e5e6531c8c5ae
João Küster <kusterzito@gmail.com>
2024-07-26 11:00:07 -0300

    feat(ui): favicon

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,21 +1,18 @@
-import { Metadata } from "next";
-import Hero from "@/components/Hero";
-import Brands from "@/components/Brands";
-import Feature from "@/components/Features";
 import About from "@/components/About";
-import FeaturesTab from "@/components/FeaturesTab";
-import FunFact from "@/components/FunFact";
-import Integration from "@/components/Integration";
+import Blog from "@/components/Blog";
+import Brands from "@/components/Brands";
+import Contact from "@/components/Contact";
 import CTA from "@/components/CTA";
 import FAQ from "@/components/FAQ";
-import Pricing from "@/components/Pricing";
-import Contact from "@/components/Contact";
-import Blog from "@/components/Blog";
-import Testimonial from "@/components/Testimonial";
+import Feature from "@/components/Features";
+import FeaturesTab from "@/components/FeaturesTab";
+import Hero from "@/components/Hero";
+import Integration from "@/components/Integration";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
-  description: "This is Home for Solid Pro",
+  title: "Eleven Soft",
+  description: "Home of Eleven Soft",
   // other metadata
 };
 
@@ -23,7 +20,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Brands />
+      {/* <Brands /> */}
       <Feature />
       <About />
       <FeaturesTab />
