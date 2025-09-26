b2b56915e3d2d181c6827d199c303c9f69903e70
Leonardo Borges <leonardo.borges@elevensoft.dev>
2025-09-26 16:06:51 -0300

    fix: rm options in menu for deploy

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -9,75 +9,14 @@ export const menuData = [
   },
   {
     id: 2,
-    title: "Plataforma",
-    path: "/plataforma",
-    newTab: false,
-  },
-  {
-    id: 3,
-    title: "Produtos",
-    newTab: false,
-    submenu: [
-      {
-        id: 31,
-        title: "ESH",
-        path: "/esh",
-        newTab: false,
-      },
-      {
-        id: 32,
-        title: "ESUT",
-        path: "/esut",
-        newTab: false,
-      },
-      {
-        id: 33,
-        title: "KEYRING",
-        path: "/keyring",
-        newTab: false,
-      },
-      {
-        id: 34,
-        title: "EL GUARDIAN",
-        path: "/el-guardian",
-        newTab: false,
-      },
-    ],
-  },
-  {
-    id: 4,
     title: "Preços",
     path: "/precos",
     newTab: false,
   },
   {
-    id: 5,
-    title: "O Jeito Elevensoft",
-    path: "/o-jeito-elevensoft",
-    newTab: false,
-  },
-  {
-    id: 6,
-    title: "Segurança",
-    path: "/seguranca",
-    newTab: false,
-  },
-  {
-    id: 7,
-    title: "Sobre",
-    path: "/sobre",
-    newTab: false,
-  },
-  {
-    id: 8,
+    id: 3,
     title: "Blog",
     path: "/blog",
-    newTab: false,
-  },
-  {
-    id: 9,
-    title: "Docs",
-    path: "/docs",
     newTab: false,
   },
 ];
