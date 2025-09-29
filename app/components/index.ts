a7841521414d7475ad9172d6c9e61f260a17411a
Willian de Souza <me@bitwilli.com>
2025-09-29 10:34:11 -0300

    fix: base project

--- app/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Header Components
-export { default as Header } from './header';
-export { ScrollProgress } from './ui/scroll-progress';
-
-// Hooks
-export { useHeader } from '../hooks/use-header';
-
-// Types
-export type { MenuItem, HeaderState, HeaderActions, HeaderHook } from '../types/header'; 
--- types/featureTab.ts
+++
@@ -1,9 +0,0 @@
-export interface FeatureTab {
-  id: number;
-  title: string;
-  subtitle: string;
-  description: string;
-  features: string[];
-  image: string;
-  link: string;
-}
